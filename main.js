b0c1e6ccf806bd446df18b2023fc947990c24297
rosealindah <76764225+rosealindah@users.noreply.github.com>
2021-08-18 16:38:05 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var array_name=[];
+function add_name(){
+var name=document.getElementById("add_name").value
+var name1=document.getElementById("add_name1").value
+var name2=document.getElementById("add_name2").value
+
+array_name.push(name);
+array_name.push(name1);
+array_name.push(name2);
+
+}
+function display_names(){
+    
+}
